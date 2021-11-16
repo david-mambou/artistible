79a5e3155e1af06b3c17da4070c445598d291dac
Jesse Alvarado <contact@jessealvarado.com>
2021-11-16 17:29:06 +0900

    implement new seed with images for services.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,7 +6,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p 'initialize seed process..'
+Service.destroy_all
+p 'removing all user accounts'
+User.destroy_all
+p 'all service instances destroyed'
 
+p 'generating fake users'
 20.times do
   name = Faker::Artist.name
   email = Faker::Internet.email(name: name)
@@ -16,16 +23,26 @@
               bio: Faker::Lorem.sentence)
 end
 
+p 'populating artists with fake details'
 User.where(artist: true).each do |user|
   2.times do
-    Service.create(user: user,
+    new_service = Service.create(user: user,
                    price: rand(2000..20_000),
                    title: Faker::Ancient.hero,
                    description: Faker::Lorem.sentence,
                    category: Faker::Hobby.activity)
+
+    file = URI.open("https://source.unsplash.com/#{rand(300..500)}x#{rand(300..500)}/?#{new_service.category}")
+    file2 = URI.open("https://source.unsplash.com/#{rand(300..500)}x#{rand(300..500)}/?#{new_service.category}")
+    file3 = URI.open("https://source.unsplash.com/#{rand(300..500)}x#{rand(300..500)}/?#{new_service.category}")
+    new_service.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+    new_service.photos.attach(io: file2, filename: 'nes.png', content_type: 'image/png')
+    new_service.photos.attach(io: file3, filename: 'nes.png', content_type: 'image/png')
+    new_service.save
   end
 end
 
+p 'populating customers with fake details'
 User.where(artist: false).each do |user|
   start_time = rand(10.years).seconds.from_now
   end_time = start_time + rand(2..6).hour
@@ -35,3 +52,5 @@ User.where(artist: false).each do |user|
                  service: Service.all.sample,
                  status: rand(0..3))
 end
+
+p 'finished seeding'
