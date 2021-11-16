f2ff8d1644a8d22e4ef747bf2a6a04ce796dd533
Jesse Alvarado <contact@jessealvarado.com>
2021-11-16 22:25:19 +0900

    improve seed process with real categories and matching images

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,7 @@ class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
+
+  CATEGORIES = ['murals', 'paintings', 'interior', 'illustrations', 'signwriting']
+  validates :category, inclusion: { in: CATEGORIES }
 end
