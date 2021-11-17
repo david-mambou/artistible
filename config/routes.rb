750f780d611ee77b8f4f208a8604a2bcafb7e9a5
David Mambou <david.mambou@gmail.com>
2021-11-17 11:54:14 +0900

    update routing tree to include both cases for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'services#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   resources :users, only: %i[index show]
-  resources :bookings, only: %i[index new create edit update] do
+  resources :services, only: %i[index show new create] do
+    resources :bookings, only: %i[new create]
+  end
+
+  resources :bookings, only: %i[index edit update] do
     resources :reviews, only: %i[edit update new create delete]
   end
-  resources :services, only: %i[index show new create]
-
   patch 'bookings/:id/accepted', to: 'bookings#accept', as: :accept_booking
   patch 'bookings/:id/declined', to: 'bookings#decline', as: :decline_booking
 end
