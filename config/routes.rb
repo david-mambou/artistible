d15283aa8b9db599f4a0db90f89fa273adaf7151
David Mambou <david.mambou@gmail.com>
2021-11-17 11:42:58 +0900

    update relations for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-
   resources :bookings, only: [:index, :new, :create, :edit, :update]
   devise_for :users
   root to: 'services#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
     collection do
-      resources :bookings, only: %i[index show update new create]
+      resources :bookings, only: %i[index show update new create] do
+        resources :reviews, only: %i[edit update new create delete]
+      end
     end
   end
 
@@ -14,4 +15,6 @@ Rails.application.routes.draw do
 
   patch 'bookings/:id/accepted', to: 'bookings#accept', as: :accept_booking
   patch 'bookings/:id/declined', to: 'bookings#decline', as: :decline_booking
+
+
 end
