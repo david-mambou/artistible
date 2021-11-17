961d39c84e26df2a5dbedde19884ff074ea8a07f
Michael Kubota <mchivas@mbm.nifty.com>
2021-11-17 12:44:22 +0900

    incremental form changes

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,32 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  const startDateInput = document.getElementById("start_time");
+  const endDateInput = document.getElementById("end_time");
+
+if (startDateInput && endDateInput) {
+  flatpickr(startDateInput, {
+    enableTime: true,
+    dateFormat: "Y-m-d H",
+    minDate: new Date().fp_incr(3),
+    maxDate: new Date().fp_incr(80),
+    onChange: function (selectedDates, selectedDate) {
+      if (selectedDate ==="") {
+        endDateInput.disabled = true;
+      }
+      }
+    }
+
+  });
+
+  flatpickr("#end_time", {
+    enableTime: true,
+    dateFormat: "Y-m-d H",
+    minDate: new Date().fp_incr(3),
+    maxDate: new Date().fp_incr(80),
+  });
+
+}
 }
 
 export { initFlatpickr };
