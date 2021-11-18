5b06c221f210829e5fdabafc8bdcb628ff93d0e4
Jesse Alvarado <contact@jessealvarado.com>
2021-11-18 19:13:20 +0900

    index page is functional minus category filters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
   def index
-    @artists = policy_scope(User).where(artist: true)
+    
+    # if params['category'].present?
+    # @artists = policy_scope(User).where(artist: true).joins(:services).where('service.category ILIKE murals')
+
+      @artists = policy_scope(User).where(artist: true)
+    # else
+    # end
   end
 
   def show
     @artist = User.find(params[:id])
     authorize @artist
   end
+  
+  def find_unique_categories(artist)
+    arr = []
+    # artists.first.services.category
+    artist.services.each do |service|
+      arr.push(service.category) unless arr.include?(service.category)
+    end
+    return arr
+  end
+
+  helper_method :find_unique_categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_021130) do
+ActiveRecord::Schema.define(version: 2021_11_18_090407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 2021_11_17_021130) do
     t.boolean "artist"
     t.text "bio"
     t.boolean "admin"
+    t.string "summary"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
