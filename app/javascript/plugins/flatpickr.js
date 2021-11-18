24121d82de0ff0e7a515aae10cf47ef25b21c20e
Michael Kubota <mchivas@mbm.nifty.com>
2021-11-18 10:25:10 +0900

    flatpick implemented

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,21 +4,25 @@ const initFlatpickr = () => {
   const startTimeInput = document.getElementById('start_time');
   const endTimeInput = document.getElementById('end_time');
 
-  flatpickr("#start_time", {
-    enableTime: true,
-    dateFormat: "Y-m-d H",
-    minDate: new Date().fp_incr(3),
-    maxDate: new Date().fp_incr(90) } )
-    // onChange: function(selectedDates, selectedDate) {
-    //   if (selectedDate === '') {
-    //     endTimeInput.disabled = true;
-    //       }
-    //     }
-    //   }
-    // )
-
-  console.log("Hello");
-}
+  if (startTimeInput && endTimeInput) {
+    flatpickr("#start_time", {
+      enableTime: true,
+      dateFormat: "Y-m-d H",
+      minDate: new Date().fp_incr(3),
+      maxDate: new Date().fp_incr(90),
+      onChange: function(selectedDates, selectedDate) {
+        if (selectedDate === '') {
+          endTimeInput.disabled = true;
+        };
+      let minDate = selectedDates[0];
+      endDate.set('minDate', minDate);
+      endTimeInput.disabled = false;
+  }})
+    const endDate =
+      flatpickr(endTimeInput, {
+        enableTime: true,
+        dateFormat: "Y-m-d H"
+});}}
 
 // const initFlatpickr = () => {
 //   flatpickr(".datepicker", {});
