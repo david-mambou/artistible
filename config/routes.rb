afb7a8f203dde038236d7e3fab716f831695b389
Jesse Alvarado <contact@jessealvarado.com>
2021-11-18 12:39:29 +0900

    create artist routes and change index to artists

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'services#index'
+  root to: 'users#artists'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :users, only: %i[index show]
+  resources :users, only: %i[show] do
+    collection do
+      get 'artists'
+    end
+  end
+
+  get 'artists/:id', to: 'users#show', as: :artist
+
   resources :services, only: %i[index show new create] do
     resources :bookings, only: %i[new create]
   end
