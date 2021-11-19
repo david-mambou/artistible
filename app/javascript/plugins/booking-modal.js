12e3fdefa5fa414c53e0265765953d9e03d4c7e4
David Mambou <david.mambou@gmail.com>
2021-11-19 12:30:50 +0900

    multiple modals working

--- app/javascript/plugins/booking-modal.js
+++ app/javascript/plugins/booking-modal.js
@@ -6,17 +6,22 @@ const showModal = (event) => {
 const initBookingModal = () => {
   const makeBooking = document.getElementById("new-booking");
   const modifyBooking = document.querySelectorAll('.modify-booking');
+  console.log(modifyBooking);
+  // use data attribute
   const cancelButton = document.getElementById("cancel-button");
 
   if (makeBooking) {
     makeBooking.addEventListener('click', (event) => {
       event.preventDefault();
-      $('.bookingModal').modal('show');
+      $('#bookingModal').modal('show');
     });
   };
 
   modifyBooking.forEach((button) => {
-    button.addEventListener('click', (event) => showModal(event));
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      $(`#booking-${button.dataset.bookingid}`).modal('show');
+    });
   });
 
   if (cancelButton) {
