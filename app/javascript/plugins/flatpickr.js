267b1deb4ef7504708e20e0e91f1573e139e20f2
David Mambou <david.mambou@gmail.com>
2021-11-19 15:08:23 +0900

    pluralize html calendar

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,29 +1,32 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-  const startTimeInput = document.getElementById('start_time');
-  const endTimeInput = document.getElementById('end_time');
+  const startTimeInput = document.querySelectorAll('.start_time');
+  const endTimeInput = document.querySelectorAll('.end_time');
 
   if (startTimeInput && endTimeInput) {
-    flatpickr(startTimeInput, {
-      enableTime: true,
-      dateFormat: "Y-m-d H:i",
-      minuteIncrement: 60,
-      minDate: new Date().fp_incr(3),
-      maxDate: new Date().fp_incr(90),
-      onChange: function(selectedDates, selectedDate) {
-        if (selectedDate === '') {
-          endTimeInput.disabled = true;
-        };
-      let minDate = selectedDates[0];
-      endDate.set('minDate', minDate);
-      endTimeInput.disabled = false;
-  }})
-    const endDate =
-      flatpickr(endTimeInput, {
+    const endDate = [];
+    startTimeInput.forEach((start, index) => {
+      flatpickr(start, {
         enableTime: true,
+        dateFormat: "Y-m-d H:i",
         minuteIncrement: 60,
-        dateFormat: "Y-m-d H:i"
+        minDate: new Date().fp_incr(3),
+        maxDate: new Date().fp_incr(90),
+        onChange: function(selectedDates, selectedDate) {
+          if (selectedDate === '') {
+            endTimeInput[index].disabled = true;
+          };
+        let minDate = selectedDates[0];
+        endDate[index].set('minDate', minDate);
+        endTimeInput[index].disabled = false;
+      }});
+      endDate[index] =
+        flatpickr(endTimeInput[index], {
+          enableTime: true,
+          minuteIncrement: 60,
+          dateFormat: "Y-m-d H:i"
+    });
 });}}
 
 // const initFlatpickr = () => {
